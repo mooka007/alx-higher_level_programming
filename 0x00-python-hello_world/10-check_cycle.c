87055c9509558f8d47ed73c66f6dcd0d20dcf9b6
mooka007 <mooka3084@gmail.com>
2023-06-05 14:53:32 -0700

    linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,24 +10,22 @@
 
 int check_cycle(listint_t *list)
 {
-	if (list == NULL)
+	listint_t *slow, *fast;
+
+	if (!list)
 	{
 		return (0);
 	}
-
-	listint_t *slow = list;
-	listint_t *fast = list;
-
-	while (fast != NULL && fast->next != NULL)
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
 		if (slow == fast)
 		{
 			return (1);
 		}
+		slow = slow->next;
+		fast = fast->next->next;
 	}
-
 	return (0);
 }
