1e8b0cb6e881d913168d2f358b33b8a7ca79f26a
mooka007 <mooka3084@gmail.com>
2023-06-06 16:05:36 -0700

    tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 # Owned By MoOka
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-        print("{}".format(chr(c - i)), end="")
-            i = 32 if i == 0 else 0
+for i in range(ord('z'), ord('a') - 1, -1):
+        print("{0}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end='')
