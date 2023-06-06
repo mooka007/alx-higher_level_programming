ba46bbf77156dab657161ab3f76d90b6b35f6832
mooka007 <mooka3084@gmail.com>
2023-06-06 09:31:25 -0700

    print comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number != 100:
-        print("{:02}".format(number), end=", ")
+        print("{:02}{}".format(number, ", " if number != 99 else ""), end="")
