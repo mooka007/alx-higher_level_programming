f1066f9366b4edff8cf49c920014bb978db3aee9
mooka007 <mooka3084@gmail.com>
2023-06-08 11:50:58 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Owned By MoOka
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+         print("{} argumenta:".format(len(argv) - 1))
+         for argument in range(1, len(argv)):
+             print("{}: {}".format(argument, argv[argument]))
