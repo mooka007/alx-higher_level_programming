35fb977966376eea5d0b6cfda5fa1874769a0bfa
mooka007 <mooka3084@gmail.com>
2023-06-08 13:15:43 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Owned By MoOka
 import hidden_4
-if __name__ = "__main__":
+if __name__ == "__main__":
     folder = dir(hidden_4)
     for name in folder:
         if name[:2] != "__":
