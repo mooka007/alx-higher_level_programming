11d8c77f24ff51b385e236c7503fb84af1b676e0
mooka007 <mooka3084@gmail.com>
2023-06-08 16:16:20 -0700

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     operator = {"+": add, "-": sub, "*": mul, "/": div}
     if argv[2] not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1) 
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
     print("{} {} {} = {}".format(a, argv[2], b, operator[argv[2]](a, b)))
