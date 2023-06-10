fd9e8ce28c8cd16ca9ee1e60c2656f560bd6915b
mooka007 <mooka3084@gmail.com>
2023-06-10 15:58:50 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/pyhton3
 # Owned by MoOka
 def no_c(my_string):
-    new_string = ""
-    for character in range(len(my_string)):
-        if my_string[character] != 'c' and my_string[character] != 'C':
-            new_string += my_string[character]
-    return new_string
+        new_string = my_string.translate({ord(i): None for i in 'cC'})
+        return new_string
