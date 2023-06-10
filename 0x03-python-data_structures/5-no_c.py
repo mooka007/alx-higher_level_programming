f2b330cf3e94cd70ab074be18f3b63bbb330260c
mooka007 <mooka3084@gmail.com>
2023-06-10 15:54:36 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     new_tox = ""
     for char in range(len(my_string)):
         if my_string[char] != 'c' and my_string[char] != 'C':
-            new_tox += my_string[char]
+            new_tox = new_tox + my_string[char]
     return new_tox
