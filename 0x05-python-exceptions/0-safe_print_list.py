6f875deb002a3a1286ab11f52587ee2d53fd6328
mooka007 <mooka3084@gmail.com>
2023-06-26 19:21:38 -0700

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Author - MoOka
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Author - Mooka
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
