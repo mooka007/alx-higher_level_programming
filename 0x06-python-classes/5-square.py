805ef75e55923210b4880ece5565069cb73bb6e4
mooka007 <mooka3084@gmail.com>
2023-06-27 15:12:23 -0700

    5 - square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,7 +3,7 @@
 """class Square that defines a square"""
 
 
-class Sqaure:
+class Square:
     """class Square that defines a square"""
 
     def __init__(self, size=0):
@@ -17,6 +17,7 @@ class Sqaure:
 
     @size.setter
     def size(self, value):
+        """property setter to set it"""
         if type(value) is int:
             if value >= 0:
                 self.__size = value
@@ -25,7 +26,7 @@ class Sqaure:
         else:
             raise TypeError("size must be an integer")
 
-    def are(self):
+    def area(self):
         """Public instance method that returns the current square area"""
         return self.__size * self.__size
 
