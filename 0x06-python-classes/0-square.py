c070c5f0fbb13bf4bbb13dcd8985fd59e63c4143
mooka007 <mooka3084@gmail.com>
2023-06-27 10:13:18 -0700

    0 / 1 - square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,7 +2,7 @@
 # Author - MoOka
 """empty class Square that defines a square"""
 
+
 class Square:
-    
     """empty class Square that defines a square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,8 +2,10 @@
 # Author - MOOka
 """class Square that defines a square"""
 
+
 class Sqaure:
     """class Square that defines a square"""
-    dedf __init__(self, size):
+
+    def __init__(self, size):
         """Instation with size"""
         self.__size = size
