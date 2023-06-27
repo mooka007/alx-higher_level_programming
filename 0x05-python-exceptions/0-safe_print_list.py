ee8dfc9c654d07aaf69274a38deb0761a72709e9
mooka007 <mooka3084@gmail.com>
2023-06-26 19:23:34 -0700

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(0, x):
+    """Print x elememts of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
-        print("")
-    except IndexError:
-        print("")
-        return i
-    return x
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
+    """Returns the division of a by b."""
     try:
-        c = a / b
-    except ZeroDivisionError:
-        c = None
-        return
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        print("Inside result: {}".format(c))
-        return c
+        print("Inside result: {}".format(div))
+    return (div)
