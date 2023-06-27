212964cedacc006381547cbf1e630d6a67a52fd5
mooka007 <mooka3084@gmail.com>
2023-06-27 13:24:00 -0700

    4 - square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# Author -MoOka
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+         self.__size = size
+
+    @property
+    def size(self):
+        """property to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter to set it"""
+        if type(vallue) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+    
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size * self.__size
+
