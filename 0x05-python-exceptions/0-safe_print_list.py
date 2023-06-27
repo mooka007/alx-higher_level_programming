548037711784d67c854df53014ab2a6bf0ddf46e
mooka007 <mooka3084@gmail.com>
2023-06-26 19:05:07 -0700

    safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Author - MoOka
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+        print("")
+    except IndexError:
+        print("")
+        return i
+    return x
