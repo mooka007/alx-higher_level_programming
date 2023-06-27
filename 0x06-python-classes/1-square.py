79cca07548d0df9cd2f18d42dfffc53fd0f4e55b
mooka007 <mooka3084@gmail.com>
2023-06-27 10:08:03 -0700

    0 / 1 - square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 # Author - MOOka
+"""class Square that defines a square"""
+
 class Sqaure:
+    """class Square that defines a square"""
     dedf __init__(self, size):
+        """Instation with size"""
         self.__size = size
