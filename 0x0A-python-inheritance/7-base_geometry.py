978572ea06e4993294928f87e8817c1200668dac
mooka007 <mooka3084@gmail.com>
2023-07-10 15:43:10 -0700

    inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Author - MoOka
+"""
+function to create basegeometry
+"""
+
+
+class BaseGeometry:
+    """Empty class"""
+
+    def area(self):
+        """Function not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
