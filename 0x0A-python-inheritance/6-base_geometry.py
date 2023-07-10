dea5bb9c52493980c20aee505d79215b7cb41dff
mooka007 <mooka3084@gmail.com>
2023-07-10 15:42:24 -0700

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Author - MoOka
+"""
+function to create basegeometry
+"""
+
+
+class BaseGeometry:
+    """Empty class"""
+
+    def area(self):
+        """Function not implemented"""
+        raise Exception("area() is not implemented")
