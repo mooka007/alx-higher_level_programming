ab3651399dc05ab308b19578e207892b21d6c7da
mooka007 <mooka3084@gmail.com>
2023-07-11 18:04:44 -0700

    from json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - MoOka
+"""convert from string to object"""
+
+import json
+
+
+def from_json_string(my_str):
+    """return data structure represented by string"""
+    return (json.loads(my_str))
