2906a0169409b88aab8f84b06f6481569e900400
mooka007 <mooka3084@gmail.com>
2023-09-17 05:46:40 -0700

    3 filter states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,10 +3,11 @@
 script that takes in arguments and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument
 """
+
 import MySQLdb
 from sys import argv
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     """
     Access to the database and get the states
     from the database.
@@ -14,24 +15,26 @@ if __name__ = '__main__':
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
+
     with db.cursor() as cur:
         cur.execute("""
-                SELECT
-                    *
-                FROM
-                    states
-                WHERE
-                    name LIKE BINARY %(name)s
-                ORDER BY
-                    states.id ASC
-            """, {
-                'name': argv[4]
-            })
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
 
-    rows = cur.fetchall()
+        rows = cur.fetchall()
 
     if rows is not None:
         for row in rows:
             print(row)
+
     cur.close()
     db.close()
