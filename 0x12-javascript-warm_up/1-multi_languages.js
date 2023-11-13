d2f1db0dd4c6f4be94b2b4380a3c9c9e716c3cb4
mooka007 <mooka3084@gmail.com>
2023-11-13 05:29:20 -0800

    warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const myVar0 = 'C is fun';
-const myVar1 = 'Python is cool';
-const myVar2 = 'JavaScript is amazing';
-console.log(myVar0);
-console.log(myVar1);
-console.log(myVar2);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i;
-for (i in phrases) {
-  console.log(phrases[i]);
-}
