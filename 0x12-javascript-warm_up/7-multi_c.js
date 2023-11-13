dc7c14dcc3e4c728b32a927b21f3277208c41a3f
mooka007 <mooka3084@gmail.com>
2023-11-13 09:05:18 -0800

    warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const num = parseInt(process.argv[2]);
-
 if (isNaN(num)){
 	console.log('Missing number of occurrences');
 } else {
@@ -11,3 +9,4 @@ if (isNaN(num)){
 		console.log(jomla);
 	}
 }
+
