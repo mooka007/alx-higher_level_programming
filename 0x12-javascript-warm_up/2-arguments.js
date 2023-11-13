241bf10b7b22507c754873385e6c10bd20b6e20f
mooka007 <mooka3084@gmail.com>
2023-11-13 06:06:50 -0800

    warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
 
+const size = process.argv.length;
+if (size < 3){
+	console.log('No argument');
+} else if (size == 3){
+	console.log('Argument found');
+} else {
+ 	console.log('Arguments found');
+}
