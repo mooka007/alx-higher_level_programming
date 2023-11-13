a945b3a7d532ffe820266bb9a301841d5e9521ba
mooka007 <mooka3084@gmail.com>
2023-11-13 05:49:59 -0800

    warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-
-// Script that prints C is fun,Python is cool and JavaScript is amazing.
-
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+const myVar0 = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'JavaScript is amazing';
+console.log(myVar0);
+console.log(myVar1);
+console.log(myVar2);
 
