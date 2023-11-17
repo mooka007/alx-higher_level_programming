8325b7ef806335ef37a79e9b86504d015c31cb00
mooka007 <mooka3084@gmail.com>
2023-11-17 14:42:33 -0800

    0-select-states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+""" Script that lists all states using MySQLdb"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    HOST = "localhost"
+    PORT = 3306
+    MY_USER = argv[1]
+    MY_PSWD = argv[2]
+    MY_DB = argv[3]
+    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
+                         db=MY_DB, port=PORT)
+    cur = db.cursor()
+    query = "SELECT * FROM states ORDER BY id"
+    cur.execute(query)
+    rowquery = cur.fetchall()
+    for q in rowquery:
+        print(q)
+    cur.close()
+    db.close()
