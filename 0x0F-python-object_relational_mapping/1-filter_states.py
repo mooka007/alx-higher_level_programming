46cadec22019f9b46a0f2d04bca0be9de1e0cde2
mooka007 <mooka3084@gmail.com>
2023-11-18 14:43:29 -0800

    update

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script that lists all states starting with N """
+import MySQLdb
+from sys import argv
+
+if __name__ = "__main__":
+    HOST = 'localhost'
+    PORT = 3306
+    MY_USER = argv[1]
+    MY_PSWD = argv[2]
+    MY_DB = argv[3]
+    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
+                         db=MY_DB, port=PORT)
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name \
+LIKE BINARY 'N%' ORDER BY id ASC"
+    cur.execute(query)
+    reo = cur.fetchall()
+    for roro in reo:
+        print(roro)
+    cur.close()
+    db.close()
+
+
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+""" Script that lists all states starting with N"""
