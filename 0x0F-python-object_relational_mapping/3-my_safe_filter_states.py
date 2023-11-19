f17a19c4eadd79a80d46dc28d0363fe10fb8410c
mooka007 <mooka3084@gmail.com>
2023-11-19 06:44:02 -0800

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" script that takes in arguments and displays all values """
+""" script that takes in arguments and displays all values"""
 
 import MySQLdb
 from sys import argv
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 
     HOST = 'localhost'
-    PORT = '3306'
+    PORT = 3306
     MY_USER = argv[1]
     MY_PSWD = argv[2]
     MY_DB = argv[3]
@@ -17,7 +17,7 @@ if __name__ = '__main__':
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (NAME,))
     row_query = cur.fetchall()
-    for r_pritn in row_query:
-        print(r_pritn)
+    for rq_print in row_query:
+        print(rq_print)
     cur.close()
     db.close()
